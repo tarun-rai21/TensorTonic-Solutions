f5e497816d879393b13bcd83c0fa6336616cf837
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-03-10 16:41:13 +0530

    Update solution for leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -5,5 +5,5 @@ def leaky_relu(x, alpha=0.01):
     Vectorized Leaky ReLU implementation.
     """
     x=np.asarray(x, dtype=float)
-    return np.where(x>=0, x, alpha*x)
+    return np.maximum(alpha*x, x)
     pass
