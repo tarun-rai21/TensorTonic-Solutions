871fd328a3147b1c62ad9c58c0fc51f2004eb5f3
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-03-21 10:59:35 +0530

    Update solution for sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -4,6 +4,8 @@ def sigmoid(x):
     """
     Vectorized sigmoid function.
     """
-    x = np.array(x, dtype=float) #converts scalars, lists and nested lists into numpy array
-    return 1/(1+np.exp(-x))
+    x = np.asarray(x, dtype=float)
+
+    sig = 1/(1+np.exp(-x))
+    return sig
     pass
